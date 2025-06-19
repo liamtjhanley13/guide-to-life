71e6aba3beb63d6ae9bd8986c2c9a19f0673d0b2
Liam Hanley <liamtjhanley@icloud.com>
2025-06-18 17:33:20 -0700

    Load ConvertKit form via ck-form.js helper

---
+++ docs/assets/ck-form.js
@@ -0,0 +1,7 @@
+(function () {
+  var s = document.createElement('script');
+  s.async = 1;
+  s.dataset.uid = '4e0e09ddbb';          // 🔁  put YOUR UID here
+  s.src = 'https://virtusguide.kit.com/4e0e09ddbb/index.js'; // 🔁  put YOUR URL here
+  document.body.appendChild(s);
+})();
